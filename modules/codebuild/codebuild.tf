c15f686cb14274e1b8ba90e5f9b65a3ad2d94536
klimnyuk <aleksey.klimnyuk@gmail.com>
2021-11-22 11:26:49 +0200

    commit

--- modules/codebuild/codebuild.tf
+++ modules/codebuild/codebuild.tf
@@ -19,7 +19,7 @@ resource "aws_codebuild_project" "example" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
+    image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
     privileged_mode = true
 
